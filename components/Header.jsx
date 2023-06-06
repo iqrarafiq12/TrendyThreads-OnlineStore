4627e00328b6f86635b120f44627dd5fc75af0a2
Iqra Rafiq <iqrak0128@gmail.com>
2023-06-06 23:18:20 +0500

    Mobile menu conditions Added

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,11 @@ import Wrapper from './Wrapper';
 import Link from 'next/link';
 import Menu from './Menu';
 
+import { IoMdHeartEmpty } from 'react-icons/io';
+import { BsCart } from 'react-icons/bs';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { VscChromeClose } from 'react-icons/vsc';
+
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const [showCatMenu, setShowCatMenu] = useState(false);
@@ -15,7 +20,7 @@ const Header = () => {
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-6 transition-transform duration-300 ${show}`}
     >
-      <Wrapper>
+      <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
           <img
             src="https://thumbs.dreamstime.com/t/green-thread-needle-eye-sewing-tailoring-tools-kit-single-icon-black-style-vector-symbol-stock-illustration-web-90351967.jpg"
@@ -23,6 +28,50 @@ const Header = () => {
           />
         </Link>
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+
+        <div className=" flex items-center gap-2 text-black">
+          {/* Icon start */}
+
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex  cursor-pointer relative justify-center items-center hover:bg-black/[0.05]">
+            <IoMdHeartEmpty className="text-[22px] md:text-[26px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 top-1 left-5 md:left-7 text-white text-[10px]
+            md:text-[12px] flex justify-center items-center px-[2px] md:[5px]"
+            >
+              5
+            </div>
+          </div>
+          {/* Icon end */}
+
+          {/* Icon start */}
+
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex  cursor-pointer relative justify-center items-center hover:bg-black/[0.05]">
+            <BsCart className="text-[20px] md:text-[26px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 top-1 left-1 md:left-7 text-white text-[10px]
+            md:text-[12px] flex justify-center items-center px-[2px] md:[5px]"
+            >
+              5
+            </div>
+          </div>
+          {/* Icon end */}
+
+          {/* mobile menu start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex  cursor-pointer relative justify-center items-center hover:bg-black/[0.05] -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+          {/* mobile menu end */}
+        </div>
       </Wrapper>
     </header>
   );
