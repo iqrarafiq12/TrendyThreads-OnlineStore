01bdf79085e03f7d352058f05b7bdda7a056d23b
Iqra Rafiq <iqrak0128@gmail.com>
2023-06-07 22:01:24 +0500

    Hero Section completed

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import HeroSection from '@/components/HeroSection';
+
 export default function Home() {
-    return <main>Home page....</main>;
+  return (
+    <>
+      <main>
+        <HeroSection />
+      </main>
+    </>
+  );
 }
