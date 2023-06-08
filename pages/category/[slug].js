c8d7f1e9331035bf32694dd738f21f917026351f
Iqra Rafiq <iqrak0128@gmail.com>
2023-06-08 21:53:23 +0500

    Dynamic Routing setup

---
+++ pages/category/[slug].js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ProductCard from '@/components/ProductCard';
+import Wrapper from '@/components/Wrapper';
+
+const Cetegory = () => {
+  return (
+    <div className="w-full md:py-20">
+      <Wrapper>
+        <div className="text-centermax-w-[800px] mt-8 md:mt-0">
+          <div className="text-[28px[ md:text-[34px] mb-5 font-semibold loading-tight">
+            New Hijab Arrives
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Cetegory;
