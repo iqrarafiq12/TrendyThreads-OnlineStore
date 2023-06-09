14396adde29eac3e42bf9af673743a6968b82200
Iqra Rafiq <iqrak0128@gmail.com>
2023-06-09 20:07:15 +0500

    File Restore

---
+++ components/ProductDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+
+function ProductDetails() {
+  return (
+    <>
+      <div>ProductDetails</div>
+    </>
+  );
+}
+
+export default ProductDetails;
