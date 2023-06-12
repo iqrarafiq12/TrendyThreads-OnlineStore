ad5861053588458cf4146876cdbee78f62d5b67e
Iqra Rafiq <iqrak0128@gmail.com>
2023-06-12 20:10:26 +0500

    New file added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import HeroSection from '@/components/HeroSection';
 
 import Wrapper from '@/components/Wrapper';
 import FeatureSection from '@/components/FeatureSection';
+import RelativeProducts from '@/components/RelativeProducts';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           <div className="flex md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
             <FeatureSection />
           </div>
+          <RelativeProducts />
         </Wrapper>
       </main>
     </>
