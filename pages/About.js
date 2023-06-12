6f7bb003ec583cb8142a2a6e62b7306a67f9bbed
Iqra Rafiq <iqrak0128@gmail.com>
2023-06-12 20:33:43 +0500

    About Section Completed

---
+++ pages/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <div className="relative mt-10  xl:container xl:mx-auto sm:flex-row sm: py-3 text-white text-[20px] mb-10 w-full max-w-[1360px] mx-auto justify-center">
+        <h2 className="max-w-md text-left mx-auto mt-4 text-3xl text-slate-900">
+          About US
+        </h2>
+
+        <p className="max-w-md text-left mx-auto mt-4 mb-6 text-slate-500">
+          Our Philosophy: Modesty is the means to all beauty - Ali. CULTURE is
+          the brand that supports contemporary and modest women from all faiths
+          seeking a modest lifestyle without having to compromise their
+          individuality or lifestyle. While providing only the finest shopping
+          experience, our aim is to inspire women who embrace the challenge we
+          face today, to combine high modern fashion and individual style while
+          never compromising the core of our faith, modesty. CULTURE Hijab Co
+          was founded by a young muslim woman, inspired by the endless
+          possibilities of modest and modern fashion. In the Summer of 2023, she
+          decided to share her vision, spread inspiration, and celebrate the
+          means to all beauty, modesty. Have a question? Get in touch! We're
+          friendly and will get back to you within 24 hours. We love to hear
+          from our customers. Your questions and comments are important to us.
+          Send us an email; TrendyThreads@gmail.com.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
